720b03f87001cf7aa50d8a69dfe75691e1448dea
Ammon21 <Ammonb6@gmail.com>
2023-06-22 11:24:00 +0300

    form completed

--- dashboard/AI webapp II/webappx.py
+++ dashboard/AI webapp II/webappx.py
@@ -23,5 +23,42 @@ grade = st.selectbox('What grade are you in?',
 mothertongue = st.selectbox('what is your first language (mother tongue)',
     ('ethiopian lang', 'foriegn language'))
 
+mothertongue = st.selectbox('Admission type inquiry',
+    ('paid', 'scholarship'))
 
-['conduct', 'age', 'gender', 'pob', 'tutor/makeup', 'grade', 'admission','mother_tongue', 'maths', 'english', 'first child', 'last child','middle child', 'authoritarian', 'authoritative', 'permissive','private', 'public', 'service', 'both', 'single']
+math = st.number_input('What is your maths grade of the previous semester')
+
+english = st.number_input('What is your english grade of the previous semester')
+
+birth = st.radio(
+    "What\'s your favorite movie genre",
+    ('First child', 'middle child', 'last child'))
+
+fx = 0
+fm = 0
+fl = 0
+
+
+if birth == 'First child':
+    fx = 1
+elif birth == 'middle child':
+    fm=1
+else:
+    fl=1    
+
+st.write(fx,fm,fl)      
+
+parenting = st.radio(
+    "How do you describe your relationship with your parents (Parrenting Style)",
+    ('Authoritative', 'Authoritarian', 'Permissive', 'Uninvolved'))
+
+transport_type = st.selectbox('What is your transport type',
+    ('Private Car', 'Public Tranportation', 'Service', 'on Foot'))
+
+
+birth = st.radio(
+    "Do you live with both parents",
+    ('yes', 'no'))
+
+
+#['conduct', 'age', 'gender', 'pob', 'tutor/makeup', 'grade', 'admission','mother_tongue', 'maths', 'english', 'first child', 'last child','middle child', 'authoritarian', 'authoritative', 'permissive','private', 'public', 'service', 'both', 'single']
